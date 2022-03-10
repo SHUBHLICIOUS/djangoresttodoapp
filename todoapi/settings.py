e2f34f5b80a71ff356dd93db185a12f8d4ecfd03
SHUBHLICIOUS <kumar.shubham1525.sk@gmail.com>
2022-03-10 12:12:01 +0530

    Added all files

--- todoapi/settings.py
+++ todoapi/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,3 +139,5 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASSES':'rest_framework.pagination.PageNumberPagination','page_size':10
 }
+
+django_on_heroku.settings(locals())
